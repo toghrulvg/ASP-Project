0bb7e7578d76c96ded8fcb0af4f1a2ca557a1a58
toghrulvg <toghrulvg@code.edu.az>
2022-11-05 01:58:02 +0400

    Front to back and Admin panel coded

---
+++ ASP-Project/Areas/AdminArea/Controllers/DashboardController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_Project.Areas.AdminArea.Controllers
+{
+    public class DashboardController : Controller
+    {
+        [Area("AdminArea")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+    }
+}
---
+++ ASP-Project/Controllers/ShopController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_Project.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
